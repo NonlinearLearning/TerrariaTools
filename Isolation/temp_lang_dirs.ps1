5db9c4df2b726664bf5faef62e22113166fed021
NonlinearLearning <2817843660@qq.com>
2026-04-21 22:03:55 +0800

    4/21

--- Isolation/temp_lang_dirs.ps1
+++
@@ -1,9 +0,0 @@
-$base = 'src/Logic/Analysis/Engine/Language'
-Get-ChildItem -Path $base -Directory -Recurse | ForEach-Object {
-  $relative = $_.FullName.Substring($PWD.Path.Length + 1)
-  $segments = ($relative -split '[\\/]') | Select-Object -Skip 5
-  $bad = $segments | Where-Object { $_ -cnotmatch '^[A-Z][A-Za-z0-9]*$' }
-  if ($bad) {
-    [PSCustomObject]@{ Path=$relative; BadSegments=($bad -join ' / ') }
-  }
-} | Format-Table -AutoSize
